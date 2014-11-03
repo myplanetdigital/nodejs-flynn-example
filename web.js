7178a0846eee68462bc11b76a21b230923ba1be2
Neil Taylor <neil.t@myplanetdigital.com>
2014-11-03 12:35:55 -0500

    Update web.js

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var port = process.env.PORT || 5000;
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello from Flynn on port '+port+' from container '+process.env.HOSTNAME+ 'on host' + os.hostname() + '\n');
+  res.end('Hello from Flynn on port '+port+' from container '+process.env.HOSTNAME+ ' with a env varaible of ' + process.env.TEST + '\n');
 }).listen(port, function() {
   console.log("Listening on " + port);
 });
